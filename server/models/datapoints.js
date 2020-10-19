f2c7d9e997a9dfc7d75f844d3ed1be38992c3024
Jesus <bcmail@gmail.com>
2020-10-19 00:14:00 -0500

    180

--- server/models/datapoints.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const DataPointSchema = new mongoose.Schema({
-    day: Number,
-    datapoint: Number,
-}, {timestamps: true});
-
-module.exports = mongoose.model('datapoints', DataPointSchema);
--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-    
-router.use('/events', require('./event'));
-router.use('/days', require('./day'));
-router.use('/datapoints', require('./datapoints'));
-
-module.exports = router;
