c89eb5634498e9f820adc6cf9ac6852d1a9c123a
Jesus <bcmail@gmail.com>
2020-10-31 14:50:20 -0500

    tttt

--- dist/js/dataTables-data.js
+++
@@ -1,1 +0,0 @@
-"use strict";$(document).ready(function(){"use strict";$('#datable_1').DataTable();$('#datable_2').DataTable({"lengthChange":false});});
--- dist/js/peity-data.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){"use strict";if($('.pie').length>0){$("span.pie").peity("pie",{width:50,height:50});}
-if($('.donut').length>0){$("span.donut").peity("donut",{width:50,height:50});}
-if($('.peity-line').length>0){$('.peity-line').each(function(){$(this).peity("line",$(this).data());});}
-if($('.peity-bar').length>0){$('.peity-bar').each(function(){$(this).peity("bar",$(this).data());});}});
